69cf9f9e65c23877d6788e145880ee2efef14d27
y33chan <y33chan@uwaterloo.ca>
2024-07-05 04:15:28 -0400

    Item spec update

--- src/app/item/item.component.spec.ts
+++ src/app/item/item.component.spec.ts
@@ -17,7 +17,48 @@ describe('ItemComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  //configuration/initilization tests
+  it('Should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should initilize items to not be editable', () => {
+    expect(component.editable).toBeFalse();
+  });
+
+  it('Should set editable to be false on save', () => {
+    //set editable to be true
+    component.editable = true;
+
+    //perform saveitem method
+    component.saveItem('testItem');
+    
+    //checks that item is no longer editable
+    expect(component.editable).toBeFalse();
+  });
+
+  it('Should save items', () => {
+    component.item.description = 'testItem';
+    //set description value
+    component.saveItem('newTestItem');
+
+    //performs checks on editability and item description
+    expect(component.editable).toBeFalse();
+    expect(component.item.description).toBe('newTestItem');
+    expect(component.item.description).not.toBe('testItem');
+  });
+
+  it('Should not save empty items edits', () => {
+    
+    //set description value
+    component.item.description = 'testItem';
+
+    //perform empty save
+    component.saveItem('');
+    
+    //performs checks on editability and item description
+    expect(component.editable).toBeFalse();
+    expect(component.item.description).toBe('testItem');
+    expect(component.item.description).not.toBe('');
   });
 });
